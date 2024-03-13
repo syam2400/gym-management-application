c4a70190d2e15afc75573019c9e815e8a0f6c7c2
SnehaAnnaSamuel <153511085+SnehaAnnaSamuel@users.noreply.github.com>
2024-03-13 16:01:05 +0530

    uiuy

--- gym_management/gym_students/models.py
+++ gym_management/gym_students/models.py
@@ -7,7 +7,7 @@ from users.models import CustomUser
 class StudentProfile(models.Model):
     student = models.ForeignKey(CustomUser,on_delete=models.CASCADE, null=True, blank=True)
     student_bio = models.TextField(null=True, blank=True)
-    student_profile_picture = models.ImageField(upload_to='media/profile_pictures/', null=True, blank=True)
+    student_profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
     is_paid = models.BooleanField(default=False)
     assigned_trainer = models.ManyToManyField('trainer.TrainerProfile', blank=True)
 
