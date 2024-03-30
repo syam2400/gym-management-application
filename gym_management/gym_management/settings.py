c934ad402c96458db084e824440ee39bea8225be
syam2400 <syam2400@gmail.com>
2024-03-30 19:58:07 +0530

    NEXT COMMIT

--- gym_management/gym_management/settings.py
+++ gym_management/gym_management/settings.py
@@ -26,16 +26,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xok*edgo3!jst@$tr-4p4k+bl%4zy88fcal!k7uxncwov!nfmy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
 INSTALLED_APPS = [
-    # 'daphne',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,13 +55,13 @@ INSTALLED_APPS = [
 
 WSGI_APPLICATION = 'gym_management.wsgi.application'
 
-# ASGI_APPLICATION = 'gym_management.asgi.application'
+ASGI_APPLICATION = 'gym_management.asgi.application'
 
-# CHANNEL_LAYERS = {
-# 	"default": {
-# 		"BACKEND": "channels.layers.InMemoryChannelLayer"
-# 	}
-# }
+CHANNEL_LAYERS = {
+	"default": {
+		"BACKEND": "channels.layers.InMemoryChannelLayer"
+	}
+}
 
 
 # backend credentials for google social auth
@@ -114,24 +114,24 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'uKEsbQbSvojhpDtYWtoHCAVLkmsvzmdL',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '30153',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'uKEsbQbSvojhpDtYWtoHCAVLkmsvzmdL',
+#         'HOST': 'roundhouse.proxy.rlwy.net',
+#         'PORT': '30153',
+#     }
+# }
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -175,8 +175,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -207,3 +207,4 @@ RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
 RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
+# py -m venv
--- gym_management/gym_management/wsgi.py
+++ gym_management/gym_management/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym_management.settings')
 
 application = get_wsgi_application()
 
-app = application
+
