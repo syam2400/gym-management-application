bab9619e1448cceb291d222338f46185cce13165
syam2400 <syam2400@gmail.com>
2024-03-31 17:07:49 +0530

    decorator commmit

--- gym_management/users/custom_decorator.py
+++ gym_management/users/custom_decorator.py
@@ -5,13 +5,18 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import resolve
 from users.models import CustomUser
 
+'''
+decorator for resisting the user's movment towards previous common pages that if he is not logged out.
+users can only see their on perosonal pages,not other users pages and common pages.  
+     
+'''
 def resist_pages(func):
     studentuser_urls = ['student_homepage','student_profile' ,'edit_user_details','chat_room_page','initiate_payments',
-                        'payment-success','payment_failed','payment_details']
-    trainer_user_urls = ['trainer_homepage','trainer_profile','trainer_details','chat_room']
+                        'payment-success','payment_failed','payment_details','contact']
+    trainer_user_urls = ['trainer_homepage','trainer_profile','trainer_details','chat_room','contact']
 
     owner_urls = ['users_list','approve_user','user_details','update_payment_status','student_details',
-                  'trainer_details','operations','assign_trainers','enquiry','view_enquiry_data','online_payment']
+                  'trainer_details','operations','assign_trainers','enquiry','view_enquiry_data','online_payment','contact']
     
     @wraps(func)
     def wrapper(request,*args, **kwargs):
